76c0c0040cbdb42cd38902948fb126aa68b02ae0
boxnos <boxnos@yahoo.com>
2014-04-28 03:23:11 +0900

    moved redpygments to bin/

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,12 @@
 #/bun/sh
-./redpygments -h
-echo test1 =======
-echo ./redpygments test/test.md
-./redpygments test/test.md
-echo test2 =======
-echo ./redpygments -s template/template.slim test/test.md
-./redpygments -s template/template.slim test/test.md
-echo test3 =======
-echo ./redpygments -s template/template.slim test/test.md -o test/out.html
-./redpygments -s template/template.slim test/test.md -o template/output.html
+bin/redpygments -h
+echo  ======= test1
+echo bin/redpygments test/test.md
+bin/redpygments test/test.md
+echo  ======= test2
+echo bin/redpygments -s template/template.slim test/test.md
+bin/redpygments -s template/template.slim test/test.md
+echo  ======= test3
+echo bin/redpygments -s template/template.slim test/test.md -o test/out.html
+bin/redpygments -s template/template.slim test/test.md -o template/output.html
 
