229d565792f1ea7a62a1b16ec46e98691e63affd
boxnos <boxnos@yahoo.com>
2014-04-29 02:10:33 +0900

    version 0.0.0.2

--- lib/redpygments.rb
+++ lib/redpygments.rb
@@ -1,5 +1,21 @@
 require "redpygments/version"
+require 'redcarpet'
+require 'pygments.rb'
 
 module Redpygments
-  # Your code goes here...
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
+  def self.parse_markdown(text)
+    options = {fenced_code_blocks: true}
+    markdown = Redcarpet::Markdown.new(HTMLwithPygments, options)
+    markdown.render text
+  end
+
+  def self.parse_slim(template, html, option={})
+    Slim::Template.new(template, option).render{ html }
+  end
 end
--- lib/redpygments/version.rb
+++ lib/redpygments/version.rb
@@ -1,3 +1,3 @@
 module Redpygments
-  VERSION = "0.0.1"
+  VERSION = "0.0.0.2"
 end
